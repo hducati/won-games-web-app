edaf4ec7cf8c701850d399f4043b818098c0b40a
Henrique Ducati <henrique.dcti@gmail.com>
2021-09-27 20:05:38 -0300

    feat: add MediaMatch component

---
+++ src/components/MediaMatch/stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+import MediaMatch from '.'
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch
+} as Meta
+
+export const Desktop: Story = () => (
+  <MediaMatch greaterThan="medium">Only on Desktop</MediaMatch>
+)
+
+export const Mobile: Story = () => (
+  <MediaMatch lessThan="medium">Only on Mobile</MediaMatch>
+)
+
+Mobile.parameters = {
+  viewPort: {
+    defaultViewPort: 'mobile1'
+  }
+}
