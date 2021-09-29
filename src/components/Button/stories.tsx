baf9170eae08e322a1c671736da4c39ab6889242
Henrique Ducati <henrique.dcti@gmail.com>
2021-09-29 08:43:46 -0300

    feat(button): added button component with size and children props

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta
+
+export const Default: Story = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy now'
+}
---
+++ src/components/Button/test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+
+import Button from '.'
+
+describe('<Button />', () => {
+  it('', () => {
+    
+  })
+})
