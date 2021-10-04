57d081b2e184c351ade738dde1261087aadec544
Henrique Ducati <henrique.dcti@gmail.com>
2021-10-04 08:43:34 -0300

    feat: created styles to Banner component

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -11,6 +11,9 @@ export default {
     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
   }
 } as Meta
 
