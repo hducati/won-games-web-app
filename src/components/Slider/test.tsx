d7296a6364edb2d808b9c1ea0e7e1fd1b2621e61
Henrique Ducati <henrique.dcti@gmail.com>
2021-10-10 17:00:18 -0300

    test(Slider): created tests and matchMedia mock

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -1,9 +1,25 @@
+import 'match-media-mock'
 import { render, screen } from '@testing-library/react'
 
 import Slider from '.'
 
 describe('<Slider />', () => {
-  it('', () => {
-    
+  it('should render children as slider item', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Item one</p>
+        <p>Item two</p>
+      </Slider>
+    )
+
+    expect(
+      screen.getByText(/item one/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    expect(
+      screen.getByText(/item two/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
