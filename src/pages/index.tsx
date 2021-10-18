1d36221e798e1cd3d9b5244f71d134e14d3ddc84
Henrique Ducati <henrique.dcti@gmail.com>
2021-10-18 20:01:18 -0300

    feat: add mock to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-export default function Index(props: any) {
+export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
 export function getStaticProps() {
   return {
     props: {
-      heading: 'OLÁ!!!!!!'
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      upComingGames: gamesMock,
+      upComingHighlight: highlightMock,
+      upComingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighlight: highlightMock
     }
   }
 }
