bcb201c10f2e82e160e49b420a3b2d441752224f
Henrique Ducati <henrique.dcti@gmail.com>
2021-12-21 07:36:05 -0300

    feat(game-item): add component

--- src/components/GameItem/stories.tsx
+++ src/components/GameItem/stories.tsx
@@ -19,5 +19,11 @@ export const WithPayment: Story<GameItemProps> = (args) => (
 )
 
 WithPayment.args = {
-  downloadLink: 'https://wongames.com/game/download/1231231'
+  downloadLink: 'https://wongames.com/game/download/1231231',
+  paymentInfo: {
+    flag: 'mastercard',
+    img: '/img/mastercard.png',
+    number: '**** **** **** 4326',
+    purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
+  }
 }
