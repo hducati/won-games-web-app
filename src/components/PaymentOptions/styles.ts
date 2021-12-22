8d613baa3de47c3e1c452963931d1ec498519b8d
Henrique Ducati <henrique.dcti@gmail.com>
2021-12-22 16:26:09 -0300

    feat(payment-options): add component with tests and stories

---
+++ src/components/PaymentOptions/styles.ts
@@ -0,0 +1,83 @@
+import { tint } from 'polished'
+import styled, { css } from 'styled-components'
+import * as ButtonStyles from 'components/Button/styles'
+import { DefaultTheme } from 'styled-components'
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+  `}
+`
+
+export const Body = styled.div`
+  ${({ theme }) => css`
+    padding: ${theme.spacings.small};
+  `}
+`
+
+export const Footer = styled.div`
+  ${({ theme }) => css`
+    background: ${tint(0.2, theme.colors.lightGray)};
+    color: ${theme.colors.black};
+    font-weight: ${theme.font.bold};
+    padding: ${theme.spacings.small};
+    display: flex;
+    align-items: center;
+
+    ${ButtonStyles.Wrapper} {
+      padding-left: ${theme.spacings.xxsmall};
+      padding-right: ${theme.spacings.xxsmall};
+      outline: 0;
+    }
+  `}
+`
+
+export const CardsList = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const CardInfo = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-right: ${theme.spacings.xxsmall};
+    }
+  `}
+`
+
+const ItemStyles = (theme: DefaultTheme) => css`
+  background: ${theme.colors.lightGray};
+  border-radius: 0.2rem;
+  color: ${theme.colors.black};
+  padding: 0 ${theme.spacings.xxsmall};
+  height: 5rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`
+
+export const CardItem = styled.div`
+  ${({ theme }) => css`
+    ${ItemStyles(theme)};
+    justify-content: space-between;
+
+    &:not(:last-child) {
+      margin-bottom: ${theme.spacings.xxsmall};
+    }
+  `}
+`
+
+export const AddCard = styled.div`
+  ${({ theme }) => css`
+    ${ItemStyles(theme)};
+
+    svg {
+      margin-left: ${theme.spacings.xxsmall};
+      margin-right: ${theme.spacings.xsmall};
+      width: 2.4rem;
+    }
+  `}
+`
