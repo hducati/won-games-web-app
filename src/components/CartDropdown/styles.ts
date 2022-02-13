a093640d83165c5104fc7b0dcde3b7fc955f3774
Henrique Ducati <henrique.dcti@gmail.com>
2022-02-13 16:12:19 -0300

    feat(CartDropdown): added component

---
+++ src/components/CartDropdown/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import { Wrapper as CartWrapper } from 'components/CartList/styles'
+
+export const Wrapper = styled.div`
+  ${CartWrapper} {
+    width: 56rem;
+  }
+`
