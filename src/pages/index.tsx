ee04fc0a545a6493df733e4b7af685eaec606f7b
Henrique Ducati <henrique.dcti@gmail.com>
2022-03-02 13:33:58 -0300

    feat(home): added graphql query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,34 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
-import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_HOME } from 'graphql/queries/home'
+import { QueryHome } from 'graphql/generated/QueryHome'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 60,
+      banners: data.banners.map((banner) => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label || null,
+        buttonLink: banner.button?.link || null,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text,
+          ribbonColor: banner.ribbon.color,
+          ribbonSize: banner.ribbon.size
+        })
+      })),
       newGames: gamesMock,
       mostPopularGames: gamesMock,
       mostPopularHighlight: highlightMock,
